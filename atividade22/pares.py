b4d917b0bcadabf0c88f6a36be14b727bf454944
MNazare <142921697+MNazare@users.noreply.github.com>
2023-11-07 16:28:22 -0300

    Add files via upload

---
+++ atividade22/pares.py
@@ -0,0 +1,7 @@
+# Exercício Python 22: Faça um programa que mostre na tela os numeros pares entre 1 e 50 (dica estude range e seus paramentros)
+
+#utilizar o for para que consiga pecorrer entre numero 1 - 50 
+
+print("os numeros pares entre 1 e 50 são:")
+for numero in range(2, 51, 2):
+    print(numero)
